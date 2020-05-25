72f2cfe0870917d653202f33e4aece908d0c2e3c
SOHYEON00 <sohyeon0amy@gmail.com>
2020-05-25 17:26:16 +0100

    이미지파일 받기, 출력,db저장,db에서 출력 모두 가능

---
+++ Search.php
@@ -0,0 +1,19 @@
+<!-- 조건에 맞는 데이터 출력-->
+
+
+<center>
+	<table width="600" border="1">
+	<tr>
+	 <td>이미지</td>
+	 <td>프로젝트 이름</td> 
+	 <td>목표 금액</td>
+	 <td>마감 날짜</td>
+	</tr>
+	</table>
+</center>
+
+<?
+include './dbconn.php';
+
+
+?>
--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?
-$conn = mysqli_connect("localhost","root","0204","world");
+$conn = mysqli_connect("localhost","root","0204","showmori");
 
 	if($conn->connect_error){
 		printf("Connect failed: %s",$conn->connect_error);
 		exit();
 	}
-?>
+?>ss
