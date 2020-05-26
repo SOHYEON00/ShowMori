bded7c3e346e5bf80882d87723746baa71782671
SOHYEON00 <sohyeon0amy@gmail.com>
2020-05-26 15:41:32 +0100

    insert 구문, 테이블 모델링txt완성/ main_page 이미지 출력

--- printImg.php
+++ printImg.php
@@ -1,40 +1,19 @@
-<!-- db안의 데이터 출력-->
 
 <?
 include './dbconn.php';
 
-	$tmpfile = $_FILES['userfile']['tmp_name'];
-	$o_name = $_FILES['userfile']['name'];
-	$filename = iconv("UTF-8","EUC-KR",$_FILES['userfile']['name']);
-	$folder = "C:/mysql-8.0.19-winx64/data/showmori/".$filename;
-	move_uploaded_file($tmpfile,$folder);
-
-	$qId = "SELECT * FROM test_img";
-	$rId = mysqli_query($conn,$qId);
-	$numId = mysqli_num_rows($rId);
-
-	$newId = $numId +1;
-
-
-	$query = "INSERT INTO test_img (id,img) VALUES ('".$newId."','".$o_name."')";
-	$result = mysqli_query($conn,$query);
-	echo "<script>alert('파일 업로드 완료');</script>";
-
-	
-	$q = "select * from test_img";
+	$q = "SELECT * FROM POST_T";
 	$r = mysqli_query($conn,$q);
 
 	while($row =mysqli_fetch_array($r)){
 		echo "
 		<table>
 			<tr>
-			<td>$row[id]</td>
-			<td><img src='./IMG/".$row['img']."'width=400,height=400></td>
+			<td>$row[S_POSTER]</td>
+			<td><img src='./data/IMG/".$row['S_POSTER']."'width=50,height=800></td>
 			</tr>
 		</table>
 		";
 	}
-
-	mysqli_close($conn);
-
+mysqli_close($conn);
 ?>
