7d72fd7d23367478f56fb5d9e124960ba41ab229
SOHYEON00 <sohyeon0amy@gmail.com>
2020-06-05 06:53:34 +0100

    table 위치 및 글씨

--- printImg.php
+++ printImg.php
@@ -1,19 +1,24 @@
-
-<?
+<? 
 include './dbconn.php';
+	
+	$qInfo = "SELECT S_TITLE,S_POSTER,S_GOALSUM,S_DEADLINE from post_t INNER JOIN S_INFO_T ON POST_T.S_PRM=S_INFO_T.S_PRM;";
+	$rPoster = mysqli_query($conn,$qInfo);
 
-	$q = "SELECT * FROM POST_T";
-	$r = mysqli_query($conn,$q);
+	
+	while($row =mysqli_fetch_array($rPoster)){
+		$title = $row['S_TITLE'];
+		$goalSum = $row['S_GOALSUM'];
+		$deadLine = $row['S_DEADLINE'];
 
-	while($row =mysqli_fetch_array($r)){
 		echo "
-		<table>
-			<tr>
-			<td>$row[S_POSTER]</td>
-			<td><img src='./data/IMG/".$row['S_POSTER']."'width=50,height=800></td>
-			</tr>
-		</table>
-		";
+			<td>
+			<img src='./data/IMG/".$row['S_POSTER']."'/>
+			<p id='s_title'>$title</p>
+			<p class='t_content'>펀딩현황 <a>$goalSum</a></p>
+			<p class='t_content'>마감날짜 <a>$deadLine</a></p>		
+			</td>";
+	
 	}
-mysqli_close($conn);
+mysqli_close($conn);															
 ?>
+
