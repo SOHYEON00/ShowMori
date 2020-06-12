d88a0d56451fbda0a3edb15cf697188b3718418a
SOHYEON00 <sohyeon0amy@gmail.com>
2020-06-12 07:42:58 +0100

    caculate s_info

--- printImg.php
+++ printImg.php
@@ -3,6 +3,7 @@ include './dbconn.php';
 	
 	$qInfo = "SELECT S_TITLE,S_POSTER,S_GOALSUM,S_DEADLINE,S_PRM from post_t;";
 	$rPoster = mysqli_query($conn,$qInfo);
+	$nDate = date('Y-m-d');
 
 
 	while($row=mysqli_fetch_array($rPoster)){
@@ -15,15 +16,24 @@ include './dbconn.php';
 		$row2 = mysqli_fetch_array($rSumMoney);
 
 		$leftSum = ($row['S_GOALSUM']-$row2['sum']);//goalsum - donated sum
-		$leftdate = $row['S_DEADLINE']; 
+		$leftDate = intval((strtotime($row['S_DEADLINE'])-strtotime($nDate)) / 86400);
 		//deadline - today
+
+		$percentage =round($row2['sum']/$row['S_GOALSUM'],2);
+
 
 		echo "
 			<td>
 			<a href='./s_info_page.html'><img src='./data/IMG/".$row['S_POSTER']."'/></a>
 			<a href='./s_info_page.html'><p id='s_title'>$title</p></a>
-			<p class='t_content'><a>$leftSum</a>남음</p>
-			<p class='t_content'><a>$leftdate</a></p>		
+			<p class='t_content'>$leftSum 남음
+				&nbsp;&nbsp;
+				$percentage %</a>
+			</p>	
+			<p class='t_content'>마감
+				&nbsp;&nbsp;&nbsp;&nbsp;
+			 	<a>D-$leftDate day</a>
+			 </p>		
 			</td>";
 	
 	}//print info of show -tuples from DB
